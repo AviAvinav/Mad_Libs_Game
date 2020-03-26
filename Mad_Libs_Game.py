042dc49694386a8ad8b2e20e55c96b96120fdd54
AviAvinav <54372449+AviAvinav@users.noreply.github.com>
2020-03-26 13:54:06 +0530

    Add files via upload

---
+++ Mad_Libs_Game.py
@@ -0,0 +1,25 @@
+print("This is a mad libs game. Fill in the blanks.")
+print("I can't believe it's already " + "___(holiday)" +"!" + "I can't wait to put on my" + "___(noun)" + "and visit every"+ "___(place)" + "in my neighbourhood. This Year, I am going to dress up as a " + "___(person)" + "with"
++ "___(adjective) ___(body part(plural))" + ". Before I " + "___(verb)" + ", I make sure to grab my " + "___(adjective) ___(noun)" + " to hold all of my " + "___(food)" + ". Finally, all of my " + "___(plural noun)" + " are ready to go!")
+
+
+
+
+
+holiday = input("Enter a holiday: ")
+noun = input("Enter a noun: ")
+place = input("Enter a place: ")
+person = input("Enter a person: ")
+adjective1 = input("Enter an adjective: ")
+body = input("Enter a body part(plural): ")
+verb = input("Enter a verb: ")
+adjective2 = input("Enter another adjective: ")
+noun0 = input("Enter another noun: ")
+food = input("Enter a food: ")
+plural_noun = input("Enter a plural noun: ")
+
+
+print("I can't believe it's already " + holiday +"!" + "I can't wait to put on my" + noun + "and visit every"+ place + "in my neighbourhood. This Year, I am going to dress up as a " + person + "with"
++ adjective1 + body + ". Before I " + verb + ", I make sure to grab my " + adjective2 + noun0 + " to hold all of my " + food + ". Finally, all of my " + plural_noun +" are ready to go!")
+
+# Mad_Libs_Game
